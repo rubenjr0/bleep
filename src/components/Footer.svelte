10721f82db91ae27f1033f11ca1ec79597affe6a
Rubén <rubenjr0@protonmail.com>
2021-08-22 19:24:16 +0200

    💄 New colors and footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,13 +1,10 @@
-<script lang="ts">
-	const links = ['Who are we', 'Contact'];
-</script>
-
-<footer class="footer">
+<footer class="footer mt-4 has-background-primary-light">
 	<div class="container has-text-centered">
-		<p>
-			Made using <a href="https://svelte.dev">Svelte</a> and
-			<a href="https://bulma.io">Bulma</a>
-			by <b>rubenjr0</b>
+		<p class="is-size-6 is-family-monospace">
+			Made using <a href="https://svelte.dev">Svelte</a>,
+			<a href="https://bulma.io">Bulma</a> and
+			<a href="https://supabase.io/">Supabase</a>
+			by <b><a href="https://github.com/rubenjr0">rubenjr0</a></b>
 		</p>
 	</div>
 </footer>
